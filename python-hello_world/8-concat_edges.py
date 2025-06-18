207ce1cb458f014d3b147976c43b6b485c8cd5bf
L-nsamba <l.nsamba@alustudent.com>
2025-06-18 09:08:14 -0700

    task8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str[39:67] + str[107:108] + str[108:109] + str[109:110] +
-       str[110:111] + str[111:112] + str[0:6])
+str = str[39:67] + str[107:108] + str[108:109] + str[109:110] + str[110:111] + str[111:112] + str[0:6]
 print(str)
