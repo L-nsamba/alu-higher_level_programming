2b569d028944a04d450bd5049b6070a0d81b1300
L-nsamba <l.nsamba@alustudent.com>
2025-07-01 08:56:22 -0700

    task2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,10 +3,12 @@
 
 
 class Square:
-    """Represents a square."""
+    """Class that defines a square."""
 
     def __init__(self, size=0):
-        """
-        Initialize a new square.
-
-        A
+        """Initialize the square with validated private size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
