a67507351f79e98ae8d585dea7e538c0768f5a13
L-nsamba <l.nsamba@alustudent.com>
2025-07-03 09:56:07 -0700

    task3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -48,7 +48,4 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return the rectangle as a string of '#' characters."""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+        """Return the rectangle as a string of '#' characters
