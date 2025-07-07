50bb5074eb33174f2ad4bb30c0d432e3abfe0920
L-nsamba <l.nsamba@alustudent.com>
2025-07-07 11:30:17 -0700

    task0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 
 """Defines a function that returns attributes and methods of an object."""
 
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object."""
     return dir(obj)
