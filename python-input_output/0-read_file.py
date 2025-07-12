ddcd224a426350ae063dbf890d0d7de504fd1d62
L-nsamba <l.nsamba@alustudent.com>
2025-07-12 08:43:24 -0700

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Read a text file (UTF-8) and print its contents to stdout.
+    """
+    Reads a text file (UTF-8) and prints it to stdout.
 
     Args:
-        filename (str): The path to the text file to be read. Defaults to empty string.
+        filename (str): The name of the file to read. Defaults to empty string.
     """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end='')
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.read()
+        print(content, end='')
