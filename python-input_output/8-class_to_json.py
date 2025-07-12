571527a44c40f0017be651d8357128d99737361f
L-nsamba <l.nsamba@alustudent.com>
2025-07-12 09:10:44 -0700

    task8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""
+Module that provides class to JSON dictionary conversion functionality.
+"""
+
+
 def class_to_json(obj):
-    """Returns the dictionary description
-       for JSON serialization of an object.
+    """
+    Returns the dictionary description with simple data structure
+    for JSON serialization of an object.
 
     Args:
-        obj: An instance of a class with serializable attributes.
+        obj: An instance of a Class with serializable attributes.
 
     Returns:
-        dict: A dictionary containing all
-        serializable attributes of the object.
+        dict: Dictionary representation of the object's attributes.
     """
     return obj.__dict__
