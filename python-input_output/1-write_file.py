c22e0ed87801e11df4bf3cfad49592c65096fcb3
L-nsamba <l.nsamba@alustudent.com>
2025-07-12 08:56:19 -0700

    task1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,11 +6,14 @@ Module that provides file writing functionality.
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF-8) and returns the number of characters written.
+    Writes a string to a text file (UTF-8) and
+    returns the number of characters written.
 
     Args:
-        filename (str): The name of the file to write to. Defaults to empty string.
-        text (str): The text to write to the file. Defaults to empty string.
+        filename (str): The name of the file to write to.
+        Defaults to empty string.
+        text (str): The text to write to the file.
+        Defaults to empty string.
 
     Returns:
         int: The number of characters written to the file.
