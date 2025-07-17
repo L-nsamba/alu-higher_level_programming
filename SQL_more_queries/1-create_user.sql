854e73776e498c0629b0790da6a6c680f5d2a998
L-nsamba <l.nsamba@alustudent.com>
2025-07-17 09:18:44 -0700

    deletions

--- SQL_more_queries/1-create_user.sql
+++
@@ -1,8 +0,0 @@
--- Create user if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges on all databases and tables
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Apply changes
-FLUSH PRIVILEGES;
