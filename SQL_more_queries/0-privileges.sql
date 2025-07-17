496154f261440b1d8e9df0f134d50798ffdd54a5
L-nsamba <l.nsamba@alustudent.com>
2025-07-17 09:05:45 -0700

    task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,3 @@
--- Create user_0d_1 and user_0d_2 if they don't exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant root-level privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Create the database user_2_db if it doesn't exist
-CREATE DATABASE IF NOT EXISTS user_2_db;
-
--- Grant only SELECT and INSERT on user_2_db to user_0d_2
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Show privileges for both users
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
