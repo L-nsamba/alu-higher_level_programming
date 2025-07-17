4adcfda136438f9e168d3bcc1ef81ac54666e5e9
L-nsamba <l.nsamba@alustudent.com>
2025-07-17 08:52:27 -0700

    task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,16 @@
--- Create the users if they don't already exist
+-- Create user_0d_1 and user_0d_2 if they don't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
--- Show privileges for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Grant root-level privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Show privileges for user_0d_2
+-- Create the database user_2_db if it doesn't exist
+CREATE DATABASE IF NOT EXISTS user_2_db;
+
+-- Grant only SELECT and INSERT on user_2_db to user_0d_2
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
+-- Show privileges for both users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
