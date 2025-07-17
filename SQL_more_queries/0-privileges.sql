4d7f07544afc2765eabc54ae2650d9be5af0fe2e
L-nsamba <l.nsamba@alustudent.com>
2025-07-17 08:39:26 -0700

    task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Script to show privileges for user_0d_1 and user_0d_2
--- This will show privileges if users exist, or error if they don't
+-- 0-privileges.sql
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
