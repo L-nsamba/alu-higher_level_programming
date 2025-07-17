d92b5236a49e0875a30316ac99c30b472386f636
L-nsamba <l.nsamba@alustudent.com>
2025-07-17 08:35:21 -0700

    task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,4 @@
--- Make sure users exist (optional, will error if user exists)
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant privileges (optional, for testing)
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT, INSERT ON *.* TO 'user_0d_2'@'localhost';
-
--- Show grants for user_0d_1
+-- Script to show privileges for user_0d_1 and user_0d_2
+-- This will show privileges if users exist, or error if they don't
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
