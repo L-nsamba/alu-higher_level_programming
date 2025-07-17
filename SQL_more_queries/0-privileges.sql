ade4b4bf0217bc75dfcf73d031258bba265dc1c6
L-nsamba <l.nsamba@alustudent.com>
2025-07-17 08:50:44 -0700

    task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,9 @@
--- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- Create the users if they don't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
