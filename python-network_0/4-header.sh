c6aa13d75982a7611de319cabd7638314bf66b17
L-nsamba <l.nsamba@alustudent.com>
2025-07-20 13:55:49 -0700

    task4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL with custom header X-HolbertonSchool-User-Id set to 98
-curl -s -H "X-HolbertonSchool-User-Id:98" "$1"
+curl -s -L -H "X-HolbertonSchool-User-Id: 98" "$1"
