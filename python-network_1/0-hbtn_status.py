fc67ac1f3857f4f1aad54544a58914615d0249c0
L-nsamba <l.nsamba@alustudent.com>
2025-07-21 12:19:08 -0700

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status using urllib"""
+"""Fetches a URL and displays the response body content, type, and UTF-8 decoding."""
 
 import urllib.request
 
-url = "https://intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+def fetch_status():
+    """Fetches and prints the body of the response from a given URL."""
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    fetch_status()
