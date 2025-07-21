c9bcc0606c7711df781ba0038c2be23e9bfb7430
L-nsamba <l.nsamba@alustudent.com>
2025-07-21 13:02:41 -0700

    updates

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """A script that
-- fetches https://intranet.hbtn.io/status.
+- fetches https://alu-intranet.hbtn.io/status.
 - uses urlib package
 """
 
+import urllib.request
+
+url = 'https://intranet.hbtn.io/status'
+if url.startswith('https://'):
+    url = 'https://alu-intranet.hbtn.io/status'
 
 if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen(url) as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
