cda1d33335deaf909cbc15876931cee351d6bdfd
L-nsamba <l.nsamba@alustudent.com>
2025-07-21 12:07:58 -0700

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """
-This module fetches https://alu-intranet.hbtn.io/status and displays the body
-response with type information and content in both bytes and UTF-8 format.
+Fetches https://alu-intranet.hbtn.io/status using urllib only
 """
 
 import urllib.request
 
+url = "https://alu-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
