1e843f4def256c24c2724e7138552c25487f6c08
L-nsamba <l.nsamba@alustudent.com>
2025-07-21 11:11:43 -0700

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"
+    url = "https://alu-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
