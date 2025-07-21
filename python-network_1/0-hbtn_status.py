de23f344ae74fd76b787e2cc7e9acb07c9b40104
L-nsamba <l.nsamba@alustudent.com>
2025-07-21 12:38:36 -0700

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,28 +6,10 @@ It displays the body response with type, content, and utf8 content information.
 import urllib.request
 
 
-if __name__ == "__main__":
-    """
-    Fetches the status from the available URL
-    and displays the response body information.
-    """
-    urls = [
-        "https://alu-intranet.hbtn.io/status",
-        "https://intranet.hbtn.io/status",
-        "http://0.0.0.0:5050/status"
-    ]
-
-    body = None
-    for url in urls:
-        try:
-            with urllib.request.urlopen(url) as response:
-                body = response.read()
-                break
-        except (urllib.error.URLError, ConnectionRefusedError, OSError):
-            continue
-
-    if body is not None:
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
