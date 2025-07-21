a8f0f749bbe7f118e4d64baa2e100c48b471ba3c
L-nsamba <l.nsamba@alustudent.com>
2025-07-21 10:58:39 -0700

    task0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches https://alu-intranet.hbtn.io/status using urllib only
+"""
+
+import urllib.request
+
+url = "https://alu-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
