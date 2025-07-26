003ecf765ce404be0d14d55bbb7e2a7aa077f5f2
L-nsamba <l.nsamba@alustudent.com>
2025-07-26 08:47:12 -0700

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,56 +1,36 @@
 #!/usr/bin/python3
-"""
-This module connects to a MySQL database and retrieves states
-that match a given name from the states table.
-"""
+""" 2-my_filter_states.py """
+
 
 import MySQLdb
 import sys
 
 
-def filter_states():
+def select_states():
+    """ lists all states that matches arvg[4]
+            from the database
     """
-    Connects to MySQL database and displays all values in the states table
-    where name matches the provided argument, sorted by states.id in
-    ascending order.
-    """
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-    
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    
-    # Create cursor object
-    cursor = db.cursor()
-    
-    # Create SQL query using format with BINARY for case sensitivity
-    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
-        state_name
-    )
-    
-    # Execute the query
-    cursor.execute(query)
-    
-    # Fetch all results
-    results = cursor.fetchall()
-    
-    # Display results
-    for row in results:
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cur = db.cursor()
+
+    sql = "SELECT * FROM states\
+        WHERE BINARY name='{:s}' ORDER BY id"\
+        .format(sys.argv[4])
+
+    cur.execute(sql)
+
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-    
-    # Close cursor and database connection
-    cursor.close()
+
+    cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    filter_states()
+    select_states()
