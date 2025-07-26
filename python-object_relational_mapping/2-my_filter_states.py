6fdb8acaff28e4e828e0931fd97fd367b8c73b93
L-nsamba <l.nsamba@alustudent.com>
2025-07-26 09:00:25 -0700

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,55 +1,56 @@
 #!/usr/bin/python3
 """
-This script searches for states matching a given name in the
-hbtn_0e_0_usa database. It takes 4 arguments: MySQL username,
-password, database name, and state name to search.
-Results are displayed sorted by state ID in ascending order.
+This module connects to a MySQL database and retrieves states
+that match a given name from the states table.
 """
 
 import MySQLdb
 import sys
 
 
-def search_states(username, password, db_name, state_name):
+def filter_states():
     """
-    Connects to MySQL database and searches for states matching the given name.
-    
-    Args:
-        username (str): MySQL username
-        password (str): MySQL password
-        db_name (str): Database name
-        state_name (str): State name to search for
+    Connects to MySQL database and displays all values in the states table
+    where name matches the provided argument, sorted by states.id in
+    ascending order.
     """
-    try:
-        # Establish database connection
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=db_name
-        )
-        
-        # Create cursor object
-        cursor = db.cursor()
-        
-        # Create and execute SQL query using format
-        query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
-        cursor.execute(query.format(state_name))
-        
-        # Fetch and display results
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-            
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-    finally:
-        # Close database connection
-        if 'db' in locals():
-            db.close()
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create cursor object
+    cursor = db.cursor()
+
+    # Create SQL query using format
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
+        state_name
+    )
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all results
+    results = cursor.fetchall()
+
+    # Display results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        search_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    filter_states()
