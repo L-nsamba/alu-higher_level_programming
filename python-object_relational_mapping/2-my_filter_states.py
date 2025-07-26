55265ed9d7dc13a599d69edf6153e3ce0ce650a2
L-nsamba <l.nsamba@alustudent.com>
2025-07-26 08:55:35 -0700

    task2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,31 +1,54 @@
 #!/usr/bin/python3
 """
-    script that takes in an argument and displays all
-    values in the states table of hbtn_0e_0_usa
+This script searches for states matching a given name in the hbtn_0e_0_usa database.
+It takes 4 arguments: MySQL username, password, database name, and state name to search.
+Results are displayed sorted by state ID in ascending order.
 """
 
-import sys
 import MySQLdb
+import sys
+
+
+def search_states(username, password, db_name, state_name):
+    """
+    Connects to MySQL database and searches for states matching the given name.
+    
+    Args:
+        username (str): MySQL username
+        password (str): MySQL password
+        db_name (str): Database name
+        state_name (str): State name to search for
+    """
+    try:
+        # Establish database connection
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=db_name
+        )
+        
+        # Create cursor object
+        cursor = db.cursor()
+        
+        # Create and execute SQL query using format
+        query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(state_name)
+        cursor.execute(query)
+        
+        # Fetch and display results
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+            
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+    finally:
+        # Close database connection
+        if 'db' in locals():
+            db.close()
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        user=sys.argv[1],
-        password=sys.argv[2],
-        db=sys.argv[3],
-        host="localhost",
-        port=3306
-    )
-    cursor = conn.cursor()
-    sql = """ SELECT * FROM states
-        WHERE name LIKE BINARY '{}'
-        ORDER BY id ASC """.format(sys.argv[4])
-
-    cursor.execute(sql)
-    data = cursor.fetchall()
-
-    for row in data:
-        print(row)
-
-    cursor.close()
-    conn.close()
+    if len(sys.argv) == 5:
+        search_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
