50e4c70ebdeea8f61e7c11723c7de279bf659b6f
L-nsamba <l.nsamba@alustudent.com>
2025-10-02 09:44:57 -0700

    task1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const filePath = process.argv[2];   // first argument → file path
-const content = process.argv[3];    // second argument → string to write
+const filePath = process.argv[2]; // first argument → file path
+const content = process.argv[3]; // second argument → string to write
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
